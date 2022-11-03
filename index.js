af4871f6df38077842263586853a8b1d967e4a8a
bookhabit <qnrgoqlt@naver.com>
2022-11-03 23:27:00 +0900

    header css UI 스타일링

---
+++ index.js
@@ -0,0 +1,7 @@
+// 검색어 input창 focus주었을 때 최근검색어창 div 보이기
+
+// 최근검색어창 토글버튼 ( 최근검색어 - 인기검색어 ) 색깔변경
+
+// 카테고리 div를 display 토글 버튼 만들기
+const toggleCategory = document.querySelector(".categoryAbout");
+console.log(toggleCategory);
