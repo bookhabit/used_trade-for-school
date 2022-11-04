ac3a0eb7f1333461425ea449635d11083166837e
bookhabit <qnrgoqlt@naver.com>
2022-11-04 23:45:56 +0900

    header 검색어창과 카테고리 숨기기 (토글 기능 구현)

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // 검색어 input창 focus주었을 때 최근검색어창 div 보이기
+const searchInput = document.querySelector(".searchInput");
+const aboutSearchContainer = document.querySelector(".aboutSearchContainer");
+
+searchInput.addEventListener("focus", showAboutSearch);
+searchInput.addEventListener("blur", hideAboutSearch);
+
+function showAboutSearch() {
+  aboutSearchContainer.style.display = "block";
+}
+
+function hideAboutSearch() {
+  aboutSearchContainer.style.display = "none";
+}
 
 // 최근검색어창 토글버튼 ( 최근검색어 - 인기검색어 ) 색깔변경
 
 // 카테고리 div를 display 토글 버튼 만들기
-const toggleCategory = document.querySelector(".categoryAbout");
-console.log(toggleCategory);
+const categoryBtn = document.querySelector(".categoryBtn");
+const categoryAboutDiv = document.querySelector(".categoryAbout");
+
+categoryBtn.addEventListener("mouseenter", showAboutCategory);
+categoryAboutDiv.addEventListener("mouseleave", hideAboutCategory);
+
+function showAboutCategory() {
+  categoryAboutDiv.style.display = "block";
+}
+
+function hideAboutCategory() {
+  categoryAboutDiv.style.display = "none";
+}
